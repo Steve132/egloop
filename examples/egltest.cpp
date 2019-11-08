c95ef8660bde97320697885f3a2b874f74d01c02
Steven B <steve@soapforge.com>
2019-11-08 15:57:11 -0500

    Stuff

--- examples/egltest.cpp
+++ examples/egltest.cpp
@@ -1,24 +1,27 @@
 #include "egloop.hpp"
-
+#include <iostream>
 
 int main(int,char**)
 {
-	std::vector<egl::Device> egldevices=egl::Device::enumerate();
+	std::vector<egloop::Device> egldevices=egloop::Device::enumerate();
 	for(size_t i=0;i<egldevices.size();i++)
 	{
 		std::cout << egldevices[i] << std::endl;
 	}
-	egl::Display offscreen_display(egldevices[0]);
+	egloop::Display offscreen_display(egldevices[0]);
 	std::cout << "The EGL version is " << offscreen_display.egl_major << "." << offscreen_display.egl_minor << std::endl;
 
-	egl::Config opengl_anyconfig(offscreen_display,{
+	
+	egloop::Config opengl_anyconfig(offscreen_display,{
 		EGL_RENDERABLE_TYPE,EGL_OPENGL_BIT
 	});
+	
 	eglBindAPI(EGL_OPENGL_API);
-	egl::Context offscreen_context(offscreen_display,opengl_anyconfig,{
+
+	egloop::Context offscreen_context(offscreen_display,opengl_anyconfig,{
 		EGL_CONTEXT_OPENGL_PROFILE_MASK,EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT,
-		EGL_CONTEXT_MAJOR_VERSION,4,
-		EGL_CONTEXT_MINOR_VERSION,5
+		EGL_CONTEXT_MAJOR_VERSION,3,
+		EGL_CONTEXT_MINOR_VERSION,1
 	});
 	return 0;
 }
